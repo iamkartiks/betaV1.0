80f2d697aa7f69a452a14bc2e90c34150eb62f74
ram.sinha <t-ramprasad.sinha@paypay-corp.co.jp>
2021-07-10 22:04:29 +0530

    update

--- blueprint/settings.py
+++ blueprint/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'app1',
     'app3',
     'app4',
+    'djangobower',
+     
 ]
 
 MIDDLEWARE = [
@@ -65,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
