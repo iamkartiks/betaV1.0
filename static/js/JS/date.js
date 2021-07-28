49c7f69987cbceab00e049ad42b08f5e2e0c8193
ram.sinha <t-ramprasad.sinha@paypay-corp.co.jp>
2021-07-28 16:26:14 +0530

    commit Co-authored-by: Ayan Ansari <objectorienteddev07@users.noreply.github.com>

--- static/js/JS/date.js
+++ static/js/JS/date.js
@@ -5,3 +5,16 @@ let footerDATE = document.querySelector('#footer-date');
 footerDATE.innerHTML = date;
 
 // console.log(date);
+
+// Header
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("scroll-head").classList.add("scroll-header")
+  } else {
+    document.getElementById("scroll-head").classList.remove("scroll-header")
+  }
+}
