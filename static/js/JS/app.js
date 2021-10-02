ae394dc2b9a64d2d8a8abb3612407a286364a4f2
Ayan Ansari <1100zaid@gmail.com>
2021-10-03 00:19:55 +0530

    Home

--- static/js/JS/app.js
+++ static/js/JS/app.js
@@ -1,37 +1,16 @@
-let DropDowns = document.querySelectorAll(".child-main");
-let CloseDiv = document.querySelector(".close-drop");
 
-function toggleDrop(e) {
-  console.log(e);
 
-  let Menu = e.target.parentElement.querySelector(".child-drop");
-  closeAllDrops(Menu);
-  if (Menu.classList.contains("show-drop")) {
-    Menu.classList.remove("show-drop");
-    CloseDiv.classList.remove("show-drop");
-  } else {
-    Menu.classList.add("show-drop");
-    CloseDiv.classList.add("show-drop");
+function switchTabs(e, tabName){
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for(i=0; i<tabcontent.length; i++){
+    tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for(i=0; i<tablinks.length; i++){
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  e.currentTarget.className += " active";
 }
 
-function closeAllDrops(Menu) {
-  let DS = document.querySelectorAll(".child-drop");
-
-  if (!(Menu instanceof HTMLElement)) {
-    DS.forEach((D) => D.classList.remove("show-drop"));
-    CloseDiv.classList.remove('show-drop');
-    return 
-  }
-  let contain = Menu.classList.contains("show-drop");
-  DS.forEach((D) => D.classList.remove("show-drop"));
-  if (contain) {
-    Menu.classList.add("show-drop");
-    CloseDiv.classList.add("show-drop");
-  }
-}
-
-DropDowns.forEach((DropDown) => {
-  DropDown.addEventListener("click", toggleDrop);
-});
-CloseDiv.addEventListener("click", closeAllDrops);
