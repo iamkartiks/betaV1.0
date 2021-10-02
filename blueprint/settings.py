19aa41b2861247e4301da5cacaab4349728303ce
ram.sinha <t-ramprasad.sinha@paypay-corp.co.jp>
2021-10-02 14:49:54 +0530

    commit

--- blueprint/settings.py
+++ blueprint/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload'
     'django.contrib.staticfiles',
     'app1',
     'app3',
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'blueprint.urls'
