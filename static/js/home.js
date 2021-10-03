91630b13351a1f17f770eddf1d4682560df3c4b2
Ayan Ansari <1100zaid@gmail.com>
2021-10-04 00:22:07 +0530

    something

--- static/js/home.js
+++ static/js/home.js
@@ -1,16 +1,5 @@
-
-function switchTabs(e, tabName){
-  let i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for(i=0; i<tabcontent.length; i++){
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for(i=0; i<tablinks.length; i++){
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabName).style.display = "block";
-  e.currentTarget.className += " active";
-}
-
-switchTabs()
+const swiper = new Swiper('.mySwiper', {
+  // Optional parameters
+  effect: "cards",
+  grabCursor: true,
+});
